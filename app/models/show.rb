f1c8344843db89a7dd5c8bbb8123d8efc69b9ca8
Jenna Ransom <jenna.ransom@gmail.com>
2019-07-16 22:59:16 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,4 +6,13 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     Show.find_by(rating: self.highest_rating)
   end 
+  
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end 
+  
+  def self.least_popular_show
+    Show.find_by(rating: self.lowest_rating)
+  end 
+    
 end 
